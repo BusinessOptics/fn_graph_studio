474a59690b44e27437c6641edd3a2e668c675435
James Saunders <james@businessoptics.biz>
2020-03-27 20:34:25 +0200

    Update version to [32mfn-graph-studio[0m [34m0.3.1[0m  and publish

--- setup.py
+++ setup.py
@@ -9,21 +9,30 @@ try:
 except ImportError:
     from distutils.core import setup
 
+import os.path
+
 readme = ''
+here = os.path.abspath(os.path.dirname(__file__))
+readme_path = os.path.join(here, 'README.rst')
+if os.path.exists(readme_path):
+    with open(readme_path, 'rb') as stream:
+        readme = stream.read().decode('utf8')
 
 setup(
     long_description=readme,
     name='fn_graph_studio',
-    version='0.3.0',
+    version='0.3.1',
     description='A web based explorer for fn_graph function composers',
     python_requires='==3.*,>=3.7.0',
+    project_urls={
+        "homepage": "https://github.com/BusinessOptics/fn_graph_studio",
+        "repository": "https://github.com/BusinessOptics/fn_graph_studio"
+    },
     author='James Saunders',
     author_email='james@businessoptics.biz',
     license='MIT',
     entry_points={
-        "console_scripts": [
-            "run_graph_studio = fn_graph_studio.cli:run_studio_command"
-        ]
+        "console_scripts": ["run_graph_studio = fn_graph_studio.cli:cli"]
     },
     packages=['fn_graph_studio', 'fn_graph_studio.examples'],
     package_dir={"": "."},
@@ -33,9 +42,9 @@ setup(
         'dash-dangerously-set-inner-html==0.*,>=0.0.2',
         'dash-interactive-graphviz==0.*,>=0.1.0',
         'dash-split-pane==1.*,>=1.0.0', 'dash-treebeard==0.*,>=0.0.1',
-        'fn-graph>=0.5.0', 'pandas>=0.25.3', 'plotly==4.*,>=4.4.0',
-        'seaborn==0.*,>=0.10.0', 'sh==1.*,>=1.0.0', 'statsmodels==0.*,>=0.11.1'
+        'fn-graph>=0.5.0', 'matplotlib==3.*,>=3.2.1', 'pandas>=0.25.3',
+        'plotly==4.*,>=4.4.0', 'seaborn==0.*,>=0.10.0', 'sh==1.*,>=1.0.0',
+        'statsmodels==0.*,>=0.11.1'
     ],
-    extras_require={
-        "dev": ["black==18.*,>=18.3.0", "matplotlib==3.*,>=3.2.1"]},
+    extras_require={"dev": ["black==18.*,>=18.3.0"]},
 )
