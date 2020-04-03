6ad1c9a261dc142fddf7b06b2af3df9b7cb4f8fb
James Saunders <james@businessoptics.biz>
2020-04-04 00:18:53 +0200

    Update version to [32mfn-graph-studio[0m [34m0.3.9[0m  and publish

--- setup.py
+++ setup.py
@@ -11,50 +11,41 @@ except ImportError:
 
 import os.path
 
-readme = ""
+readme = ''
 here = os.path.abspath(os.path.dirname(__file__))
-readme_path = os.path.join(here, "README.rst")
+readme_path = os.path.join(here, 'README.rst')
 if os.path.exists(readme_path):
-    with open(readme_path, "rb") as stream:
-        readme = stream.read().decode("utf8")
+    with open(readme_path, 'rb') as stream:
+        readme = stream.read().decode('utf8')
 
 setup(
     long_description=readme,
-    name="fn_graph_studio",
-    version="0.3.8",
-    description="A web based explorer for fn_graph function composers",
-    python_requires="==3.*,>=3.7.0",
+    name='fn_graph_studio',
+    version='0.3.9',
+    description='A web based explorer for fn_graph function composers',
+    python_requires='==3.*,>=3.7.0',
     project_urls={
         "homepage": "https://github.com/BusinessOptics/fn_graph_studio",
-        "repository": "https://github.com/BusinessOptics/fn_graph_studio",
+        "repository": "https://github.com/BusinessOptics/fn_graph_studio"
     },
-    author="James Saunders",
-    author_email="james@businessoptics.biz",
-    license="MIT",
-    entry_points={"console_scripts": ["fn_graph_studio = fn_graph_studio.cli:cli"]},
-    packages=["fn_graph_studio"],
+    author='James Saunders',
+    author_email='james@businessoptics.biz',
+    license='MIT',
+    entry_points={
+        "console_scripts": ["fn_graph_studio = fn_graph_studio.cli:cli"]
+    },
+    packages=['fn_graph_studio'],
     package_dir={"": "."},
     package_data={"fn_graph_studio": ["*.css"]},
     install_requires=[
-        "dash==1.*,>=1.7.0",
-        "dash-ace",
-        "dash-core-components==1.*,>=1.6.0",
-        "dash-cytoscape==0.*,>=0.1.1",
-        "dash-dangerously-set-inner-html==0.*,>=0.0.2",
-        "dash-interactive-graphviz==0.*,>=0.1.0",
-        "dash-split-pane==1.*,>=1.0.0",
-        "dash-treebeard==0.*,>=0.0.1",
-        "fn-graph>=0.7.2",
-        "matplotlib==3.*,>=3.2.1",
-        "networkx==2.*,>=2.4.0",
-        "pandas>=0.25.3",
-        "plotly==4.*,>=4.4.0",
-        "seaborn==0.*,>=0.10.0",
-        "sh==1.*,>=1.0.0",
-        "statsmodels==0.*,>=0.11.1",
-    ],
-    dependency_links=[
-        "git+https://github.com/BusinessOptics/dash-ace.git#egg=dash-ace"
+        'dash==1.*,>=1.7.0', 'dash-ace-persistent==0.*,>=0.3.3',
+        'dash-core-components==1.*,>=1.6.0', 'dash-cytoscape==0.*,>=0.1.1',
+        'dash-dangerously-set-inner-html==0.*,>=0.0.2',
+        'dash-interactive-graphviz==0.*,>=0.1.0',
+        'dash-split-pane==1.*,>=1.0.0', 'dash-treebeard==0.*,>=0.0.1',
+        'fn-graph>=0.7.2', 'matplotlib==3.*,>=3.2.1', 'networkx==2.*,>=2.4.0',
+        'pandas>=0.25.3', 'plotly==4.*,>=4.4.0', 'seaborn==0.*,>=0.10.0',
+        'sh==1.*,>=1.0.0', 'statsmodels==0.*,>=0.11.1'
     ],
     extras_require={"dev": ["black==18.*,>=18.3.0"]},
 )
